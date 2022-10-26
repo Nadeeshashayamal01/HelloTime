4134d0d1e7208c7c9668831889f038f0de5e44ed
Saulo Alexandre de Barros <31544944+DinoSaulo@users.noreply.github.com>
2022-10-26 10:36:01 -0300

    Refactoring Time.java

--- Time.java
+++ Time.java
@@ -1,12 +1,12 @@
 public class Time {
     public static void main(String[] args) {
-     double time = 12.05;
-      if (time <= 11.59) {
-        System.out.println(" Hi ! Good morning.");
-      } else if (time < 18) {
-        System.out.println("Hi ! Good afternoon.");
-      }  else {
-        System.out.println("Hi ! Good night.");
-      }
+        double time = 12.05;
+        if (time <= 11.59) {
+            System.out.println(" Hi ! Good morning.");
+        } else if (time < 18) {
+            System.out.println("Hi ! Good afternoon.");
+        } else {
+            System.out.println("Hi ! Good night.");
+        }
     }
-  }
+}
