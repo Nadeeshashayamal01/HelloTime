d19e86b1f27594b6232589a2821a8a265d785784
JanithPM99 <93361642+JanithPM99@users.noreply.github.com>
2022-10-27 07:37:51 +0530

    Update Time.java

--- Time.java
+++ Time.java
@@ -2,11 +2,11 @@ public class Time {
     public static void main(String[] args) {
      double time = 12.05;
       if (time <= 11.59) {
-        System.out.println(" Hi ! Good morning.");
+        System.out.println(" Hi amazing individual ! Good morning.");
       } else if (time < 18) {
-        System.out.println("Hi ! Good afternoon.");
+        System.out.println("Hi amazing individual! Good afternoon.");
       }  else {
-        System.out.println("Hi ! Good night.");
+        System.out.println("Hi amazing individual ! Good night.");
       }
     }
   }
